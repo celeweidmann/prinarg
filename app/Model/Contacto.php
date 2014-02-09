1a781f6a4f6447e8e849c6ee3e72756d87143190
cele <celeweidmann@gmail.com>
2014-02-09 11:45:51 -0300

    Contacto.

--- app/Model/Contacto.php
+++ app/Model/Contacto.php
@@ -4,8 +4,7 @@ class Contacto extends AppModel {
     public $name = 'Contacto';
     public $useTable = false;
     public $validate = array(
-        'subject' => 'notEmpty',
-        'email' => array (
+    	'email' => array (
             'rule'          => 'email',
             'message'       => 'Debe escribir una dirección de email válida.'
         ),
@@ -14,18 +13,16 @@ class Contacto extends AppModel {
             'allowEmpty'    => false,
             'message'       => 'Debe escribir su nombre.'
         ),
-        'mensaje' => array(
+        'consulta' => array(
             'rule'          => 'notEmpty',
             'allowEmpty'    => false,
-            'message'       => 'Debe escribir un mensaje a enviar.'
+            'message'       => 'Debe escribir una consulta a enviar.'
         ),
     );
 
     var $_schema = array(
         'email'     => array('type'=>'string', 'length'=>100),
         'nombre'    => array('type'=>'string', 'length'=>100), 
-        'subject'   => array('type'=>'string', 'length'=>100), 
-        'mensaje'   => array('type'=>'text', 'length'=>800),
-        'consulta' => array(),
+        'consulta'   => array('type'=>'text', 'length'=>800),
     );
 }
